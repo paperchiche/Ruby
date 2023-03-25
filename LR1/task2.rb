0c9d9031e5c91fc9b009794a8b7ed7b9a7c0cc9a
paperchiche <20artyom02@mail.ru>
2023-03-25 05:41:41 +0300

    task2 (method2, v7)

--- LR1/task2.rb
+++ LR1/task2.rb
@@ -7,6 +7,7 @@ def simple(digit)
   return true
 end
 
+
 def sum_simple(digit)
   sum = 0
   for i in 2..digit do
@@ -17,7 +18,21 @@ def sum_simple(digit)
   return sum
 end
 
+
+def count(digit)
+  k = 0
+  while digit != 0
+    s = digit % 10
+    if(s % 2 != 0 and s > 3)
+      k += 1
+    end
+    digit /= 10
+  end
+  return k
+end
+
 puts 'Введите число: '
 num = gets.to_i
 
 puts "Сумма простых делителей числа: #{sum_simple(num)}"  # Сумма простых делителей числа
+puts "Количество нечетных цифр числа, больших 3: #{count(num)}" # Количество нечетных цифр числа, больших 3
