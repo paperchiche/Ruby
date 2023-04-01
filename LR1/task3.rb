373b234cb8235846359ad6b1fb6e20486748e6b1
paperchiche <20artyom02@mail.ru>
2023-04-01 09:40:27 +0300

    task3 is done

---
+++ LR1/task3.rb
@@ -0,0 +1,37 @@
+def minimal_el(arr)
+  min = arr[0]
+  for i in 0..arr.length-1.to_i
+    if arr[i]<min
+      min=arr[i]
+    end
+  end
+  return min
+end
+
+def first_positive_el(arr)
+  for i in 0..arr.length-1.to_i
+    return i if arr[i]>0
+  end
+  return "Нет такого числа"
+end
+
+if ARGV.count < 2
+  puts "Отсутствуют аргументы"
+  return
+end
+
+method = ARGV[0]
+path = ARGV[1]
+
+file = File.open(path)
+array = file.readline.split(' ').map(&:to_i)
+
+puts "Массив, #{array}\n"
+case method
+when "1"
+  puts "Минимальный элемент: #{minimal_el(array)}"
+when "2"
+  puts "Номер первого положительного элемента: #{first_positive_el(array)}"
+else
+  puts "Метода с таким номером нет"
+end
