fe94530c6a600d908eb4a0b3966a619112415a43
paperchiche <20artyom02@mail.ru>
2023-04-01 10:02:30 +0300

    task4.7

---
+++ LR1/task4.rb
@@ -0,0 +1,14 @@
+def cyclic_shift_2_right(array) #7 - сдвигает элементы массива на две позиции вправо
+  return array[0..-3].unshift(array[-2])
+end
+
+
+def select_method(num_method, file_name) #функция принимает два значения номер функция и имя файла (путь) с массивом чисел возвращает значение выбраной функции указаного массива 
+  array = IO.read(file_name).split.map(&:to_i)
+  case num_method
+  when "1"
+    return cyclic_shift_2_right(array)
+  end
+end
+
+puts select_method(ARGV[0], ARGV[1])
