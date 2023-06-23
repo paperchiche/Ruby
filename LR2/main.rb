ead6b942988898a7ef1522fef15058d0df802221
paperchiche <20artyom02@mail.ru>
2023-06-23 14:32:47 +0300

    исправление конструктора (3-5)

--- LR2/main.rb
+++ LR2/main.rb
@@ -1,5 +1,5 @@
 require_relative 'student'
 
-student = Student.new(0,"Иванов", "Иван", "Иванович", "+7924361345", "iii", "ii@mail.ru", "iii")
+student = Student.new(name:"Иванов", surname:"Иван", father_name:"Иванович", id:23)
 puts student.to_s
 
