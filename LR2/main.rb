24ab49425d4ffd2002218845ba1df1925b32be50
paperchiche <20artyom02@mail.ru>
2023-06-23 15:02:31 +0300

    добавил метод set_contacts (задание 10)

--- LR2/main.rb
+++ LR2/main.rb
@@ -1,5 +1,7 @@
 require_relative 'student'
 
-student = Student.new(name:"Иванов", surname:"Иван", father_name:"Иванович", id:23, phone: "+7-928-431-55-44")
+student = Student.new(name:"Иванов", surname:"Иван", father_name:"Иванович", id:23, phone: "+7-928-431-41-43", tg:"@D-rk_mo", git:"@SMDPSKM")
+puts student.to_s
+puts student.validate
 
 
