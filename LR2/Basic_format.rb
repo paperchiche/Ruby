9d95673d0a0a87010ce5334a2883a949ce3f147e
paperchiche <20artyom02@mail.ru>
2023-08-09 15:25:42 +0300

    Student_list_controller

--- LR2/Basic_format.rb
+++ LR2/Basic_format.rb
@@ -1,4 +1,5 @@
 class Basic_format
+  protected
   def format_to_hash(str)
 
   end
@@ -6,4 +7,25 @@ class Basic_format
   def hash_to_format(hash)
 
   end
+end
+
+def read_file(rfile: )
+  file = File.open(rfile, "r")
+  return file.read
+end
+
+def write_file(wfile: , data:)
+  file_text = File.open(wfile, "w")
+  file_text.write(data)
+  file_text.close
+end
+
+public
+def file_data_to_hash(rfile:)
+  return format_to_hash(str: read_file(rfile: rfile))
+end
+
+def hash_to_file(wfile: , hash:)
+  write_file(wfile:wfile, data: hash_to_format(hash:hash))
+  return nil
 end
